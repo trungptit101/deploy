8fae86e6f6a2dea2c67b5ef18fa7f1f85d54c2d6
trungnt <trungptit7@gmail.com>
2024-04-22 23:13:48 +0700

    a

---
+++ assets/EImg-f0d9a1a1.js
@@ -0,0 +1,1 @@
+import{_ as a,bw as s}from"./index-43fa284a.js";const c=a(s,[["__scopeId","data-v-2aa5ccff"]]);export{c as E};
---
+++ assets/Home-2e4f7b91.js
@@ -0,0 +1,1 @@
+import{d as a,o,a as t,c as r,u as s}from"./index-43fa284a.js";const m=a({__name:"Home",setup(n){const e=s();return o(async()=>await e.replaceByPath("/dashboard")),(c,u)=>(t(),r("div",null,"sdfsdf"))}});export{m as default};
